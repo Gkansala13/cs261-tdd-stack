496a701abec397f9d5e425cff74ec7bde36ea4aa
Grayson Kansala <kansalag@oregonstate.edu>
2020-10-25 21:02:42 -0700

    All Tests working!

--- stack.py
+++ stack.py
@@ -1,7 +1,27 @@
 # Stack: A stack.
 # Your implementation should pass the tests in stack.py.
-# YOUR NAME
+# Grayson Kansala
 
 class Stack:
 
-    pass
+    def __init__(self):
+        self.stack=[]
+
+    def is_empty(self):
+        return len(self.stack)==0
+            
+    def pop(self):
+        if self.is_empty():
+            raise IndexError('Index is empty')
+        else:
+            return self.stack.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError('Index is empty')
+        else:
+            return self.stack[-1]
+    
+    def push(self, value):
+        self.stack.append(value)
+    
